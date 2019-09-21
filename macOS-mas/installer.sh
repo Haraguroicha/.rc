3027ad1895a7709d4a90bbdf6919cc36dc304b61
腹黒い茶 <haraguroicha@tipsy.coffee>
2019-09-21 23:38:58 +0800

    add app installer for mas

---
+++ macOS-mas/installer.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+i () {
+	id=$1
+	name=$2
+	echo "Install for `$name` (`$id`) ..."
+	mas install $id
+}
+
+i 638161122	"YubiKey Personalization Tool"
+i 425264550	"Blackmagic Disk Speed Test"
+i 539883307	"LINE"
+i 409183694	"Keynote"
+i 409203825	"Numbers"
+i 409201541	"Pages"
+i 497799835	"Xcode"
+i 747648890	"Telegram"
+i 425424353	"The Unarchiver"
+i 1037126344	"Apple Configurator 2"
+i 1295203466	"Microsoft Remote Desktop"
+i 803453959	"Slack"
+
