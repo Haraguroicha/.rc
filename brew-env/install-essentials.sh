5ec48fb6bc0819eee9dd0f4baaa45a2a11610ae4
腹黒い茶 <haraguroicha@tipsy.coffee>
2019-09-21 23:34:50 +0800

    add homebrew installer and disable analytics

--- brew-env/install-essentials.sh
+++ brew-env/install-essentials.sh
@@ -4,6 +4,10 @@ OSTYPE=`uname`
 
 cd "$(realpath $(dirname "$0"))"
 
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+brew analytics off
+
 case $OSTYPE in
 	Darwin)
 	./install_darwin.sh
