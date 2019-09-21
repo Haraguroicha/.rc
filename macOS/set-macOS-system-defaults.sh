6741c7841a80f32b629bd136e7d08effca80ffa3
腹黒い茶 <haraguroicha@tipsy.coffee>
2019-09-21 23:44:44 +0800

    add system defaults for macOS and move mas installer

---
+++ macOS/set-macOS-system-defaults.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Avoid creating .DS_Store files on network volumes
+defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true
+
+# Enable the Develop menu and the Web Inspector in Safari
+defaults write com.apple.Safari IncludeInternalDebugMenu -bool true && \
+defaults write com.apple.Safari IncludeDevelopMenu -bool true && \
+defaults write com.apple.Safari WebKitDeveloperExtrasEnabledPreferenceKey -bool true && \
+defaults write com.apple.Safari com.apple.Safari.ContentPageGroupIdentifier.WebKit2DeveloperExtrasEnabled -bool true && \
+defaults write NSGlobalDomain WebKitDeveloperExtras -bool true
+
+# Show build duration for Xcode
+defaults write com.apple.dt.Xcode ShowBuildOperationDuration YES
