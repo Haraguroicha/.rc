7329ea8bebe85e0e6be3861a27aa95943e1d95e6
腹黒い茶 <haraguroicha@tipsy.coffee>
2022-01-21 14:20:42 +0800

    add init oh-my-zsh environment script

---
+++ .init-oh-my-zsh-env.zsh
@@ -0,0 +1,40 @@
+#!/bin/zsh
+
+DIR=$( cd "$( dirname "$0" )" && pwd )
+
+$DIR/link-config.sh
+
+ZSH_PLUGINS_DIR=${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins
+ZSH_THEMES_DIR=${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes
+
+[ ! -d "$HOME/.oh-my-zsh" ] && sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+(
+    cd "$ZSH_PLUGINS_DIR"
+    for repo in https://github.com/marlonrichert/zsh-autocomplete \
+        https://github.com/zsh-users/zsh-autosuggestions \
+        https://github.com/zsh-users/zsh-history-substring-search \
+        https://github.com/zsh-users/zsh-syntax-highlighting \
+    ; do
+        repo_name=$(echo "$repo" | awk -F/ '{print $NF}')
+        if [ ! -d "$repo_name" ]; then
+            git clone --depth=1 $repo
+        else
+            cd $repo_name
+            git pull
+        fi
+    done
+    cd "$ZSH_THEMES_DIR"
+    for repo in https://github.com/romkatv/powerlevel10k \
+    ; do
+        repo_name=$(echo "$repo" | awk -F/ '{print $NF}')
+        if [ ! -d "$repo_name" ]; then
+            git clone --depth=1 $repo
+        else
+            cd $repo_name
+            git pull
+        fi
+    done
+)
+
+source ~/.zshrc
