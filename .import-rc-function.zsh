4052e6476ff39eda026056ef459be9194092dd80
腹黒い茶 <haraguroicha@tipsy.coffee>
2022-01-26 04:30:55 +0800

    force default import rc to self if not link zshrc

--- .import-rc-function.zsh
+++ .import-rc-function.zsh
@@ -1,6 +1,9 @@
 #!/bin/zsh
 
-MY_RC_PATH=$(dirname $(readlink ~/.zshrc))
+DIR=$( cd "$( dirname "$0" )" && pwd )
+MY_RC_PATH=$(dirname $(readlink ~/.zshrc) 2>/dev/null)
+
+[ -z "${MY_RC_PATH}" ] && MY_RC_PATH=$DIR
 
 import-rc-function () {
     for file in $(find "${MY_RC_PATH}/.rc.function" -name \*.zsh); do
