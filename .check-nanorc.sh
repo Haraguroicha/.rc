33e45d5216ff090430060f13fe292c129e8167ca
腹黒い茶 <haraguroicha@tipsy.coffee>
2022-01-30 17:35:21 +0800

    update .sheldon

---
+++ .check-nanorc.sh
@@ -0,0 +1,12 @@
+#!/bin/zsh
+
+DIR=$(dirname $(readlink ~/.zshrc))
+
+if [ ! "$(readlink ~/.nano)" = "${SHELDON_DIR}/repos/github.com/scopatz/nanorc" ]; then
+  rm ~/.nano
+  ln -sf "${SHELDON_DIR}/repos/github.com/scopatz/nanorc" ~/.nano
+fi
+if [ ! "$(readlink ~/.nanorc)" = "$HOME/.nano/nanorc" ]; then
+  rm ~/.nanorc
+  ln -sf ~/.nano/nanorc ~/.nanorc
+fi
