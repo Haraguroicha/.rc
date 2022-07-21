11458c302131ed1f46392a90f1dbfa29cd47737f
腹黒い茶 <haraguroicha@tipsy.coffee>
2022-07-21 19:01:17 +0800

    add docker and podman alias

---
+++ .rc.function/docker.zsh
@@ -0,0 +1,17 @@
+#!/bin/zsh
+
+OLD_DOCKER_BIN=$(which docker)
+
+docker-native () {
+	(
+		$OLD_DOCKER_BIN $*
+	)
+}
+
+docker () {
+	(
+		#export DOCKER_HOST=$(podman system connection list --format=json | jq -r '.[] | select(.Default) | .URI' | awk -F'/run' '{print $1}')
+		export DOCKER_HOST=unix://${HOME}/.local/share/containers/podman/machine/podman-machine-default/podman.sock
+		podman $*
+	)
+}
