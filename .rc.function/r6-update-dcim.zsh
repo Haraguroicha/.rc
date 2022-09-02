6dcf851983111e596fac06362bbe86a966f5e1d3
腹黒い茶 <haraguroicha@tipsy.coffee>
2022-09-02 18:58:29 +0800

    update r6 dcim with progress

--- .rc.function/r6-update-dcim.zsh
+++ .rc.function/r6-update-dcim.zsh
@@ -8,15 +8,27 @@ r6-update-dcim () {
 		cd /Volumes/${FOLDER}/DCIM
 
 		mvtodate () {
-			d=$(exiftool -j "$1" | jq -r '.[].DateTimeOriginal | split(" ")[0] | gsub(":"; "")')
-			[ ! -d "/Volumes/${FOLDER}/DCIM/${d}" ] && echo mkdir -p "/Volumes/${FOLDER}/DCIM/${d}" && mkdir -p "/Volumes/${FOLDER}/DCIM/${d}"
-			echo mv "$1" "/Volumes/${FOLDER}/DCIM/${d}"
-			mv "$1" "/Volumes/${FOLDER}/DCIM/${d}"
+			if [ -n "$1" ]; then
+				d=$(exiftool -j "$1" | jq -r '.[].DateTimeOriginal | split(" ")[0] | gsub(":"; "")')
+				[ ! -d "/Volumes/${FOLDER}/DCIM/${d}" ] && echo mkdir -p "/Volumes/${FOLDER}/DCIM/${d}" && mkdir -p "/Volumes/${FOLDER}/DCIM/${d}"
+				echo mv "$1" "/Volumes/${FOLDER}/DCIM/${d}"
+				mv "$1" "/Volumes/${FOLDER}/DCIM/${d}"
+			else
+				echo "empty imput"
+			fi
 		}
+
+		list="$(find . -maxdepth 2 -type f -ipath '*/1*canon/*')"
+		total=$(echo "${list}" | wc -l | sed 's/ //g')
+		processes=0
 
 		while read p
 		do
-			mvtodate $p
-		done <<<"$(find . -maxdepth 2 -type f -ipath '*/1*canon/*')"
+			if [ -n "$p" ]; then
+				processes=$(($processes + 1))
+				echo -ne "Process ${processes}/${total}\t-> "
+				mvtodate $p
+			fi
+		done <<<"${list}"
 	)
 }
