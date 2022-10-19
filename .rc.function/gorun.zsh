754ba56a999e86258fb31a3967c5971f76f81fba
腹黒い茶 <haraguroicha@tipsy.coffee>
2022-10-19 12:35:23 +0800

    set dirty flag when go run build

--- .rc.function/gorun.zsh
+++ .rc.function/gorun.zsh
@@ -1,9 +1,13 @@
 #!/bin/zsh
 
 function g() {
+	set -Eexo pipefail
+	export DIR=$( cd "$( dirname "$0" )" && pwd )
 	export TIMESTAMP=$(date -u '+%Y-%m-%d_%H:%M:%S_%Z')
+	export DIRTY_FLAG="$([ "$(git diff --stat)" != "" ] && echo "-dirty")"
 	export VERSION=$(git describe --tag)
 	export HASH=$(git rev-parse HEAD)
+	export SHORT_HASH=$(git rev-parse --short=8 HEAD)
 	export TAG_VERSION=$(git describe --tags --long --dirty --always)
 	export LD_FLAGS=`(cat << FLAG
 -s
@@ -11,7 +15,7 @@ function g() {
 -X main.appVersion=${VERSION}
 -X main.buildDate=${TIMESTAMP}
 -X main.commitHash=${HASH}
--X main.tagName=${TAG_VERSION}
+-X main.tagName=${TAG_VERSION}${DIRTY_FLAG}
 FLAG
 ) | xargs`
 	go run -ldflags "${LD_FLAGS} -v" . "$@"
