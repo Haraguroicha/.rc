fdbacfa9765c6bd7c8eea7e49ae271ff84afb300
腹黒い茶 <haraguroicha@tipsy.coffee>
2023-04-06 16:16:27 +0800

    add mm for mtr dns.google in tmux as a dashboard

---
+++ .rc.function/mm.zsh
@@ -0,0 +1,19 @@
+#!/usr/bin/env zsh
+
+mm () {
+	SN=`hostname`-mtr-google
+	P_TITLE="mtr to dns.google"
+	tmux new -d "sleep 1"
+	tmux has -t $SN 2> /dev/null
+	if [[ $? != 0 ]]; then
+		tmux new -s $SN -n $P_TITLE -d "echo 'starting to mtr dns v4'; sleep .25; while true; do sudo mtr -n4 dns.google; sleep 1; done" \; \
+			splitw -h -p 50 "echo 'starting to mtr ipv4'; sleep .75; while true; do sudo mtr -n 8.8.8.8; sleep 1; done" \; \
+			lastp \; \
+			splitw -v -p 50 "echo 'starting to mtr dns v6'; sleep .5; while true; do sudo mtr -n6 dns.google; sleep 1; done" \; \
+			lastp \; \
+			setw synchronize-panes\; \
+			attach
+	else
+		tmux a -t $SN
+	fi
+}
