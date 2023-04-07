0ab31670a0c4eab32057b1e62321b218e9c3735c
腹黒い茶 <haraguroicha@tipsy.coffee>
2023-04-07 19:59:34 +0800

    fix for gp include interface link-local gateway occurred unknown host error

--- .rc.function/gp.zsh
+++ .rc.function/gp.zsh
@@ -5,6 +5,9 @@ gp () {
 		shift
 		gping $*
 	else
-		gping $(netstat -nr | grep default | grep -v tun | awk '{print $2}' | xargs) $(dig dns.google +short) $(dig dns.google aaaa +short) $*
+		gping $(netstat -nr | grep default | egrep -v '%|#' | awk '{print $2}' | xargs) \
+			$(dig dns.google +short) \
+			$(dig dns.google aaaa +short) \
+			$*
 	fi
 }
