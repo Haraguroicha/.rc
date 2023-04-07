0658269809ec55c285eda1dc8d9c1974721c6fa2
腹黒い茶 <haraguroicha@tipsy.coffee>
2023-04-07 20:21:09 +0800

    add for default dns server into gping

--- .rc.function/gp.zsh
+++ .rc.function/gp.zsh
@@ -6,6 +6,7 @@ gp () {
 		gping $*
 	else
 		gping $(echo -e "$(netstat -nr | grep default | egrep -v '%|#' | awk '{print $2}')
+$(ipconfig getoption "" domain_name_server)
 $(traceroute -n -w1 -m1 -q1 1.1.1.1 2>&1 | grep -v traceroute | awk '{print $2}')
 $(traceroute -n -w1 -m1 -q1 8.8.8.8 2>&1 | grep -v traceroute | awk '{print $2}')
 1.1.1.1
