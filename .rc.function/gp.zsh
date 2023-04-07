666f010dac4213b7637714683ff0bbec89593e27
腹黒い茶 <haraguroicha@tipsy.coffee>
2023-04-07 20:16:33 +0800

    add for detect wireguard gateway into gping

--- .rc.function/gp.zsh
+++ .rc.function/gp.zsh
@@ -5,9 +5,12 @@ gp () {
 		shift
 		gping $*
 	else
-		gping $(netstat -nr | grep default | egrep -v '%|#' | awk '{print $2}' | xargs) \
-			$(dig dns.google +short) \
-			$(dig dns.google aaaa +short) \
-			$*
+		gping $(echo -e "$(netstat -nr | grep default | egrep -v '%|#' | awk '{print $2}')
+$(traceroute -n -w1 -m1 -q1 1.1.1.1 2>&1 | grep -v traceroute | awk '{print $2}')
+$(traceroute -n -w1 -m1 -q1 8.8.8.8 2>&1 | grep -v traceroute | awk '{print $2}')
+1.1.1.1
+$(dig one.one.one.one aaaa +short)
+$(dig dns.google +short)
+$(dig dns.google aaaa +short)" | uniq | xargs) $*
 	fi
 }
