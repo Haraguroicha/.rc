7053d65b7a2ca603d2411a99ca58c6f8483a28db
腹黒い茶 <haraguroicha@tipsy.coffee>
2023-04-07 18:06:41 +0800

    update gp for ability to append more args

--- .rc.function/gp.zsh
+++ .rc.function/gp.zsh
@@ -1,5 +1,10 @@
 #!/usr/bin/env zsh
 
 gp () {
-	gping $(netstat -nr | grep default | grep -v tun | awk '{print $2}' | xargs) $(dig dns.google +short) $(dig dns.google aaaa +short)
+	if [[ "$1" == "--" ]]; then
+		shift
+		gping $*
+	else
+		gping $(netstat -nr | grep default | grep -v tun | awk '{print $2}' | xargs) $(dig dns.google +short) $(dig dns.google aaaa +short) $*
+	fi
 }
