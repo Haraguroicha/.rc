9d947dd6b2715c22f3229959c8905204b7523247
腹黒い茶 <haraguroicha@tipsy.coffee>
2023-10-04 15:51:05 +0800

    add az completion

---
+++ .rc.function/az.zsh
@@ -0,0 +1,11 @@
+#!/bin/zsh
+
+if [ -f /usr/local/etc/bash_completion.d/az ]; then
+	autoload -U +X bashcompinit && bashcompinit
+	source /usr/local/etc/bash_completion.d/az
+fi
+
+if [ -f /opt/homebrew/etc/bash_completion.d/az ]; then
+	autoload -U +X bashcompinit && bashcompinit
+        source /opt/homebrew/etc/bash_completion.d/az
+fi
