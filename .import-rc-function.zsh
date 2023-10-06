64635ade4bbd7fab76de6d20c856b977984442c4
腹黒い茶 <haraguroicha@tipsy.coffee>
2023-10-06 15:30:04 +0800

    using eval instead of source rc function script and export as global variable for some tools can fork them to use in isolated non-interactive shell

--- .import-rc-function.zsh
+++ .import-rc-function.zsh
@@ -6,9 +6,12 @@ MY_RC_PATH=$(dirname $(readlink ~/.zshrc) 2>/dev/null)
 [ -z "${MY_RC_PATH}" ] && MY_RC_PATH=$DIR
 
 import-rc-function () {
+    _MY_RC_FUNCS=()
     for file in $(find "${MY_RC_PATH}/.rc.function" -name \*.zsh); do
         if [[ -a "$file" ]]; then
-            source "$file"
+            _MY_RC_FUNCS+=("$(cat "$file")")
         fi
     done
+    export MY_RC_FUNCS="${(@)_MY_RC_FUNCS}"
+    eval "${MY_RC_FUNCS}"
 }
