ee53d29f1f8dc67bffd8f847b97d4cefefbd23db
腹黒い茶 <haraguroicha@tipsy.coffee>
2023-12-04 18:19:15 +0800

    allow multiple url input as arguments, and auto wrap into stdin to aria2c

--- .rc.function/aa2c.zsh
+++ .rc.function/aa2c.zsh
@@ -1,6 +1,34 @@
 #!/usr/bin/env zsh
 
+_get_urls () {
+}
+
+__aa () {
+	aria2c -j64 -s16 -x16 --file-allocation=none --remote-time --disable-ipv6 --continue --summary-interval=5 "$@"
+}
+
 aa2c () {
 	set -x
-	aria2c -j64 -s16 -x16 --file-allocation=none --remote-time --disable-ipv6 --continue --summary-interval=5 $*
+	args=()
+	urls=()
+	while [ $# -ne 0 ]; do
+		arg=$1
+		shift
+		case $arg in
+			--)
+			shift
+			urls=( $@ )
+			echo -n "$urls[@]"
+			break
+			;;
+			*)
+			args+=$arg
+			;;
+		esac
+	done
+	if [[ "$#urls[@]" -gt "0" ]]; then
+		__aa -i- "${(@)args}" <<<"${(F)urls}"
+	else
+		__aa "${(@)args}"
+	fi
 }
