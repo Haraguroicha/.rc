2043ea1196b4a0191444f6cba7b6ff69548924e6
腹黒い茶 <haraguroicha@tipsy.coffee>
2024-07-22 18:08:52 +0800

    add ability to check default route and en0 route ip

--- .rc.function/myip.zsh
+++ .rc.function/myip.zsh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
 myip () {
-	yq '... comments = "" | ... style = ""' <(curl -sSL wc.sg)
+	default_route_ip=$(yq '... comments = "" | ... style = ""' <(curl -sSL wc.sg))
+	first_interface_ip=$(yq '... comments = "" | ... style = ""' <(curl --interface en0 -sSL wc.sg))
+	if [[ "$default_route_ip" != "$first_interface_ip" ]]; then
+		yq '.default = .ip-info | del(.ip-info)' <(echo "$default_route_ip")
+		yq '.first-ip = .ip-info | del(.ip-info)' <(echo "$first_interface_ip")
+	else
+		yq '' <(echo "$default_route_ip")
+	fi
 }
