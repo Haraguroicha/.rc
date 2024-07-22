269cdc2b7fdcb0e068e1d2548b87ae046f039c44
腹黒い茶 <haraguroicha@tipsy.coffee>
2024-07-22 18:31:29 +0800

    force init completions for eza

--- .rc.function/ls.zsh
+++ .rc.function/ls.zsh
@@ -1,11 +1,18 @@
 #!/usr/bin/env zsh
 
-_ls () {
-	if [[ -x "$(which eza)" ]]; then
-		eza --icons --bytes --group --git --extended "$@"
-	else
-		/bin/ls -hG --color=always "$@"
-	fi
-}
+[[ -x "$(which eza)" ]] && fpath=("$(brew --prefix eza)/share/zsh/site-functions" $fpath) && compinit
 
-alias ls='_ls'
+#_ls () {
+#	if [[ -x "$(which eza)" ]]; then
+#		eza --icons --bytes --group --git --extended "$@"
+#	else
+#		/bin/ls -hG --color=always "$@"
+#	fi
+#}
+#
+#alias ls='_ls'
+
+if [[ -x "$(which eza)" ]]; then
+	alias ls='eza --icons --bytes --group --git --extended'
+	alias l='ls -lab'
+fi
