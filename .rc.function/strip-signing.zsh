b49e94bcdd60475335adc00a4a32c5c0cf46e72e
腹黒い茶 <haraguroicha@tipsy.coffee>
2024-07-23 09:36:47 +0800

    strip signing and also reformat file for readable

--- .rc.function/strip-signing.zsh
+++ .rc.function/strip-signing.zsh
@@ -1,5 +1,6 @@
 #!/usr/bin/env zsh
 
 strip-signing () {
-	openssl smime -inform DER -verify -in $1 -noverify -out $1
+	openssl smime -inform DER -verify -in "$1" -noverify -out "$1"
+	plutil -convert xml1 "$1"
 }
