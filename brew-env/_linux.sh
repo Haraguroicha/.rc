d3216e76a3076425db2909ece5199fae2b6a1e8d
腹黒い茶 <haraguroicha@tipsy.coffee>
2024-08-18 16:34:35 +0800

    add for add font on steamos

--- brew-env/_linux.sh
+++ brew-env/_linux.sh
@@ -57,28 +57,46 @@ brew install \
 #	heroku \
 #	adoptopenjdk \
 
-if [[ ! -L "/usr/local/share/fonts" ]]; then
-	sudo rm -f /usr/local/share/fonts/.uuid
-	sudo rm -f /usr/local/share/fonts/*
-	sudo rmdir /usr/local/share/fonts
+IS_STEAMOS=0
+LSB_RELEASE=$(which lsb_release)
+if [[ -n "${LSB_RELEASE}" ]]; then
+  if [[ "$(${LSB_RELEASE} -i | grep SteamOS | awk -F: '{print $NF}' | xargs)" == "SteamOS" ]]; then
+    IS_STEAMOS=1
+  fi
 fi
-sudo ln -sf /home/linuxbrew/.linuxbrew/share/fonts /usr/local/share/
-sudo fc-cache -fv
+if [[ "${IS_STEAMOS}" == "1" ]] then
+  TARGET_FONT_ROOT=$(sudo bash -c 'echo "$HOME"')/.local/share/
+  if [[ ! -L "${TARGET_FONT_ROOT}/fonts" ]]; then
+    sudo rm -f "${TARGET_FONT_ROOT}/fonts/.uuid"
+    sudo rm -f "${TARGET_FONT_ROOT}/fonts/*"
+    sudo rmdir "${TARGET_FONT_ROOT}/fonts"
+  fi
+  sudo ln -sf /home/linuxbrew/.linuxbrew/share/fonts "${TARGET_FONT_ROOT}"
+  sudo fc-cache -fv
+else
+  if [[ ! -L "/usr/local/share/fonts" ]]; then
+    sudo rm -f /usr/local/share/fonts/.uuid
+    sudo rm -f /usr/local/share/fonts/*
+    sudo rmdir /usr/local/share/fonts
+  fi
+  sudo ln -sf /home/linuxbrew/.linuxbrew/share/fonts /usr/local/share/
+  sudo fc-cache -fv
 
-# install 1Password GUI
-if [[ ! -f "/etc/apt/sources.list.d/1password.list" ]]; then
-	curl -sS https://downloads.1password.com/linux/keys/1password.asc | \
-		sudo gpg --dearmor --output /usr/share/keyrings/1password-archive-keyring.gpg
-	echo 'deb [arch=amd64 signed-by=/usr/share/keyrings/1password-archive-keyring.gpg] https://downloads.1password.com/linux/debian/amd64 stable main' | \
-		sudo tee /etc/apt/sources.list.d/1password.list
-	sudo mkdir -p /etc/debsig/policies/AC2D62742012EA22/
-	curl -sS https://downloads.1password.com/linux/debian/debsig/1password.pol | \
-		sudo tee /etc/debsig/policies/AC2D62742012EA22/1password.pol
-	sudo mkdir -p /usr/share/debsig/keyrings/AC2D62742012EA22
-	curl -sS https://downloads.1password.com/linux/keys/1password.asc | \
-		sudo gpg --dearmor --output /usr/share/debsig/keyrings/AC2D62742012EA22/debsig.gpg
-	sudo apt update && sudo apt install -y 1password 1password-cli policykit-1-gnome
-	sudo groupadd onepassword-cli
-	sudo chown root:onepassword-cli "$(which op)" && \
-	sudo chmod g+s "$(which op)"
+  # install 1Password GUI
+  if [[ ! -f "/etc/apt/sources.list.d/1password.list" ]]; then
+    curl -sS https://downloads.1password.com/linux/keys/1password.asc | \
+      sudo gpg --dearmor --output /usr/share/keyrings/1password-archive-keyring.gpg
+    echo 'deb [arch=amd64 signed-by=/usr/share/keyrings/1password-archive-keyring.gpg] https://downloads.1password.com/linux/debian/amd64 stable main' | \
+      sudo tee /etc/apt/sources.list.d/1password.list
+    sudo mkdir -p /etc/debsig/policies/AC2D62742012EA22/
+    curl -sS https://downloads.1password.com/linux/debian/debsig/1password.pol | \
+      sudo tee /etc/debsig/policies/AC2D62742012EA22/1password.pol
+    sudo mkdir -p /usr/share/debsig/keyrings/AC2D62742012EA22
+    curl -sS https://downloads.1password.com/linux/keys/1password.asc | \
+      sudo gpg --dearmor --output /usr/share/debsig/keyrings/AC2D62742012EA22/debsig.gpg
+    sudo apt update && sudo apt install -y 1password 1password-cli policykit-1-gnome
+    sudo groupadd onepassword-cli
+    sudo chown root:onepassword-cli "$(which op)" && \
+    sudo chmod g+s "$(which op)"
+  fi
 fi
