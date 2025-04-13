5b6739fee1d07e0f6be71ebd0dfcd9d086a532ec
腹黒い茶 <haraguroicha@tipsy.coffee>
2025-04-13 17:03:14 +0800

    add recursive gitgc for start from pwd and display current path

--- .rc.function/gitgc.zsh
+++ .rc.function/gitgc.zsh
@@ -3,10 +3,23 @@
 gitgc () {
 	(
 		cd "$(git root)"
+		echo "Current at '$(pwd)'"
 		\ls -lah .git/objects/pack
 		find .git/objects
 		git rp
 		\ls -lah .git/objects/pack
 		find .git/objects
+	)
+}
+
+gitrgc () {
+	(
+		CD=$(pwd)
+		for g in $(find . -type d -name .git | sed -e 's#/.git##g')
+		do
+			cd "$g"
+			gitgc
+			cd "$CD"
+		done
 	)
 }
