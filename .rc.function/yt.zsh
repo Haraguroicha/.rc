7b0bb8b5bc9615cc6685c853cd31f508d4e69ca1
腹黒い茶 <haraguroicha@tipsy.coffee>
2025-08-14 00:58:44 +0800

    add yt-vod for download membership vod by using browser cookies

--- .rc.function/yt.zsh
+++ .rc.function/yt.zsh
@@ -8,3 +8,11 @@ yt () {
 		--downloader-args aria2c:"-j64 -s16 -x16 --file-allocation=none --remote-time --continue --summary-interval=5" \
 		"$@"
 }
+
+yt-vod () {
+	test -e "$(dirname $(readlink ~/.zshrc))/.rc.function/.yt.env" && source "$(dirname $(readlink ~/.zshrc))/.rc.function/.yt.env"
+	[[ -n "${BROWSER_PROFILE}" ]] && yt --sub-langs live_chat --sub-format ass/srt/best --embed-subs --write-subs --write-auto-subs \
+		--write-info-json --write-description --write-comments \
+		--cookies-from-browser "${BROWSER_PROFILE}" \
+		"$@"
+}
