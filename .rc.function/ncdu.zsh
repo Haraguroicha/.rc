1371fbbd543aaec840c6d05f319a208a07b6cc09
腹黒い茶 <haraguroicha@tipsy.coffee>
2025-11-24 12:04:58 +0800

    exclude additional folders for ncdu

--- .rc.function/ncdu.zsh
+++ .rc.function/ncdu.zsh
@@ -2,8 +2,11 @@
 
 ncdu () {
   sudo $(brew --prefix)/bin/ncdu \
+    --exclude /System/Volumes \
     --exclude /System/Volumes/Data \
     --exclude /Volumes \
+    --exclude "/System/Volumes/Data/home/Library/CloudStorage" \
+    --exclude "/System/Volumes/Data/home/Library/Mobile Documents" \
     --exclude "$HOME/Library/CloudStorage" \
     --exclude "$HOME/Library/Mobile Documents" \
     --si \
