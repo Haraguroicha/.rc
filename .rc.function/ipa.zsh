beda031295da9f3189fe3815095054ae09ef6747
腹黒い茶 <haraguroicha@tipsy.coffee>
2026-06-04 03:38:16 +0800

    update ipa path

--- .rc.function/ipa.zsh
+++ .rc.function/ipa.zsh
@@ -1,7 +1,7 @@
 #!/usr/bin/env zsh
 
 ipa () {
-	GEM=$(brew --prefix ruby@3.0)/bin/gem
+	GEM=$(brew --prefix ruby@3.4)/bin/gem
 	IPA=$("${GEM}" env | grep 'INSTALLATION DIRECTORY' | grep -v USER | awk -F: '{print $NF}' | xargs echo)/bin/ipa
 	[[ -x "${GEM}" ]] && [[ -x "${IPA}" ]] && "${IPA}" "$@"
 }
